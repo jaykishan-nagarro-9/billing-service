0a34346539e3dad5326d54abab14daed45d429db
jaykishanmehta <jaykishan.mehta@nagarro.com>
2024-08-05 11:48:48 +0530

    Discount interface

---
+++ src/main/java/com/example/demo/billing/core/Discountable.java
@@ -0,0 +1,33 @@
+package com.example.demo.billing.core;
+
+import com.example.demo.pojo.Cart;
+
+/**
+ * This interface helps designing discount configuration in the system. <br>
+ * It has some common methods that can be used to calculate discount.
+ */
+public interface Discountable {
+
+	/**
+	 * Calculate discount based on configuration
+	 * @return
+	 */
+	public double calculateDiscount(Cart cart);
+	
+	/**
+	 * Show case custom product description
+	 * @return discount description
+	 */
+	public String discountDescription();
+	
+	/**
+	 * Calculates percentage value
+	 * 
+	 * @param productPrice
+	 * @param discount
+	 * @return
+	 */
+	default double percentageOfProductValue(double productPrice, int discount) {
+		return (productPrice * discount)/100;
+	}
+}
